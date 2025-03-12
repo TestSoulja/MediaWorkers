0e905536c2df0ec226e33352fd50c18fc1efb0dd
TestSoulja <serzh.ekcker@yandex.ru>
2025-03-12 08:17:04 +0300

    modified:   VideoDownloader/RutubeDownloader.py

--- VideoDownloader/RutubeDownloader.py
+++ VideoDownloader/RutubeDownloader.py
@@ -4,14 +4,18 @@ import os
 s = os.path.abspath(__file__)
 c = s.replace(os.path.basename(os.path.abspath(__file__)), '')
 
-links = ["https://rutube.ru/video/e3a32b3f95f31697ef6886cbaf593a6f/",
-         "https://rutube.ru/video/b54105442173ccf5f5f309e175e43c49/",
-         "https://rutube.ru/video/9da22dac41bca1b3b342acbc2094d977/",
-         "https://rutube.ru/video/67d14a48d82b7254630141bc23a808f7/",
-         "https://rutube.ru/video/434c29e794da0426b285995501fec61f/",
-         "https://rutube.ru/video/03064a86780ab58c0ef466fd9f287a51/",
-         "https://rutube.ru/video/930eee0a808ec62ebb357580e5e7817f/",
-         "https://rutube.ru/video/0dfacac9c6f024ef6c50b61bb0f4daf9/"]
+links = ["https://rutube.ru/video/f93b891f523fb3fa3429c8e7dd37578c/?playlist=525381",
+         "https://rutube.ru/video/6fcbe836d7b255d122a97daeb5ad219d/?playlist=525381",
+         "https://rutube.ru/video/ae79c4cb9432cb4e4a59f69801c59fe7/?playlist=525381",
+         "https://rutube.ru/video/893ccbce38e557288c190b78fe2dc9cf/?playlist=525381",
+         "https://rutube.ru/video/421ce09dc28d36b2b2d04d4d899c4d15/?playlist=525381",
+         "https://rutube.ru/video/44dc343561bac543c22ed0fa86d0af6c/?playlist=525381",
+         "https://rutube.ru/video/8dbef0758cbd9df033aaced57e9a12a8/?playlist=525381",
+         "https://rutube.ru/video/1ea38dd435f8bef78896dc5128e8b6c6/?playlist=525381",
+         "https://rutube.ru/video/e96633e4ad3ffbca95c858771b894648/?playlist=525381",
+         "https://rutube.ru/video/b358ac3e14941fde49035df99ca21aa8/?playlist=525381",
+         "https://rutube.ru/video/a62b3d97280de3f2825a4101f7e8aee5/?playlist=525381",
+         "https://rutube.ru/video/e4edaee1c8f2cce2edec6451da17c5c2/?playlist=525381"]
 
 # Get a list of videos
 # Each object is the same video but with different resolution
