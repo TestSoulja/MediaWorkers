4e6e40d763e182aa6b4c758ca082d93b5d5099ab
TestSoulja <serzh.ekcker@yandex.ru>
2025-03-14 06:37:54 +0300

    modified:   VideoDownloader/RutubeDownloader.py

--- VideoDownloader/RutubeDownloader.py
+++ VideoDownloader/RutubeDownloader.py
@@ -4,18 +4,16 @@ import os
 s = os.path.abspath(__file__)
 c = s.replace(os.path.basename(os.path.abspath(__file__)), '')
 
-links = ["https://rutube.ru/video/f93b891f523fb3fa3429c8e7dd37578c/?playlist=525381",
-         "https://rutube.ru/video/6fcbe836d7b255d122a97daeb5ad219d/?playlist=525381",
-         "https://rutube.ru/video/ae79c4cb9432cb4e4a59f69801c59fe7/?playlist=525381",
-         "https://rutube.ru/video/893ccbce38e557288c190b78fe2dc9cf/?playlist=525381",
-         "https://rutube.ru/video/421ce09dc28d36b2b2d04d4d899c4d15/?playlist=525381",
-         "https://rutube.ru/video/44dc343561bac543c22ed0fa86d0af6c/?playlist=525381",
-         "https://rutube.ru/video/8dbef0758cbd9df033aaced57e9a12a8/?playlist=525381",
-         "https://rutube.ru/video/1ea38dd435f8bef78896dc5128e8b6c6/?playlist=525381",
-         "https://rutube.ru/video/e96633e4ad3ffbca95c858771b894648/?playlist=525381",
-         "https://rutube.ru/video/b358ac3e14941fde49035df99ca21aa8/?playlist=525381",
-         "https://rutube.ru/video/a62b3d97280de3f2825a4101f7e8aee5/?playlist=525381",
-         "https://rutube.ru/video/e4edaee1c8f2cce2edec6451da17c5c2/?playlist=525381"]
+links = ["https://rutube.ru/video/f9d5af87c94d112990dcf9137c72873d/",
+         "https://rutube.ru/video/09218c1821cd41771750ebdde76dcbf6/",
+         "https://rutube.ru/video/cef02bdcb723eb06ae759f3f7af7d7a9/",
+         "https://rutube.ru/video/a2cf2517ffe99dd9be4837aaaac43699/",
+         "https://rutube.ru/video/cb00d00340ddec3479cc767547ff89b4/",
+         "https://rutube.ru/video/029c3cb92b19b5e9c86ccfb0de316dcc/",
+         "https://rutube.ru/video/f4d124043f55fccbbcf30d958c0ccb62/",
+         "https://rutube.ru/video/0d32dc8c9d6705b795cdf0b97a5c32d0/",
+         "https://rutube.ru/video/b670c87292f965ce2a6ac59ae7b76066/",
+         "https://rutube.ru/video/31713ff51680e5b7b2e536e520ce930e/"]
 
 # Get a list of videos
 # Each object is the same video but with different resolution
