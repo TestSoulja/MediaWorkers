bd8fa77c60972de7b05a87b9b1e06372d4d553a1
TestSoulja <serzh.ekcker@yandex.ru>
2025-03-19 10:16:55 +0300

    modified:   VideoDownloader/RutubeDownloader.py

--- VideoDownloader/RutubeDownloader.py
+++ VideoDownloader/RutubeDownloader.py
@@ -4,16 +4,7 @@ import os
 s = os.path.abspath(__file__)
 c = s.replace(os.path.basename(os.path.abspath(__file__)), '')
 
-links = ["https://rutube.ru/video/f9d5af87c94d112990dcf9137c72873d/",
-         "https://rutube.ru/video/09218c1821cd41771750ebdde76dcbf6/",
-         "https://rutube.ru/video/cef02bdcb723eb06ae759f3f7af7d7a9/",
-         "https://rutube.ru/video/a2cf2517ffe99dd9be4837aaaac43699/",
-         "https://rutube.ru/video/cb00d00340ddec3479cc767547ff89b4/",
-         "https://rutube.ru/video/029c3cb92b19b5e9c86ccfb0de316dcc/",
-         "https://rutube.ru/video/f4d124043f55fccbbcf30d958c0ccb62/",
-         "https://rutube.ru/video/0d32dc8c9d6705b795cdf0b97a5c32d0/",
-         "https://rutube.ru/video/b670c87292f965ce2a6ac59ae7b76066/",
-         "https://rutube.ru/video/31713ff51680e5b7b2e536e520ce930e/"]
+links = ["https://rutube.ru/video/ed6e7261292892cf3f9a59cf2c4e72d7/"]
 
 # Get a list of videos
 # Each object is the same video but with different resolution
